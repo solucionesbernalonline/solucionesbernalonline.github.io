3cca604ddf8b0c24ddb0055e8dfe4e69dba0e102
solucionesbernalonline <solucionesbernalonline@gmail.com>
2025-12-15 16:32:07 -0300

    Delete src/main.js

--- src/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js - CORREGIR NOMBRE
-import './styles/animate.css';
-import './styles/bootstrap.css';
-import './styles/flexslider.css';
-import './styles/style.css';
-import './styles/styleIcomoon.css';
-
-console.log('✅ CSS cargado via Vite');
